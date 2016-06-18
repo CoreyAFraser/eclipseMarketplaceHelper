388d76f69c9dec828b7751940f37f3ef3ee61b0c
Corey Fraser <CoreyAFraser@gmail.com>
2016-06-18 02:40:44 -0400

    change port for server

--- app.js
+++ app.js
@@ -135,7 +135,7 @@ errDomain.run(function() {
 	  	io.emit('publishTech', compressedTechnology);
   	}
 
-	var server = http.listen(4444, function() {
+	var server = http.listen(80, function() {
 		console.log("Express server running");
 	});
 
